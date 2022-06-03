e90b583dc41c3c2d77e8ccbcac1afb2b39f524d4
vitormenezess <vitormenezes43@icloud.com>
2022-06-03 16:05:51 -0300

    render 03/06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+
+import "./App.css";
 
 const App = () => {
-  return <h1> Hello word!</h1>;
+  // let message = "Hello word!";
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar Progrmação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ver filme",
+      completed: true,
+    },
+    {
+      id: "3",
+      title: "Ler Livros",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container">
+        <Tasks tasks={tasks}/>
+      </div>
+    </>
+  );
 };
 export default App;
